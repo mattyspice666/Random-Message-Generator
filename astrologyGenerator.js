c109e570a29c6717a281c514944621b9124cf1d3
mattyspice666 <96106005+mattyspice666@users.noreply.github.com>
2022-01-17 00:59:09 -0700

    cleaned up code slightly

--- astrologyGenerator.js
+++ astrologyGenerator.js
@@ -9,7 +9,7 @@ astrologySigns = [
   "Virgo",
   "Libra",
   "Scorpio",
-  "Sagitarius",
+  "Sagittarius",
   "Capricorn",
   "Aquarius",
   "Pisces",
@@ -17,30 +17,31 @@ astrologySigns = [
 
 /* zodiacSign takes date as (month, day) and generates the zodiac sign associated */
 
-function zodiacSign(month, day) {
-  if ((month == 1 && day < 20) || (month == 12 && day >= 22)) {
+function zodiacSign(month,
+                    day) {
+  if ((month === 1 && day < 20) || (month === 12 && day >= 22)) {
     return astrologySigns[9];
-  } else if ((month == 2 && day < 19) || (month == 1 && day >= 20)) {
+  } else if ((month === 2 && day < 19) || (month === 1 && day >= 20)) {
     return astrologySigns[10];
-  } else if ((month == 3 && day < 21) || (month == 2 && day >= 19)) {
+  } else if ((month === 3 && day < 21) || (month === 2 && day >= 19)) {
     return astrologySigns[11];
-  } else if ((month == 4 && day < 20) || (month == 3 && day >= 21)) {
+  } else if ((month === 4 && day < 20) || (month === 3 && day >= 21)) {
     return astrologySigns[0];
-  } else if ((month == 5 && day < 21) || (month == 4 && day >= 20)) {
+  } else if ((month === 5 && day < 21) || (month === 4 && day >= 20)) {
     return astrologySigns[1];
-  } else if ((month == 6 && day < 21) || (month == 5 && day >= 21)) {
+  } else if ((month === 6 && day < 21) || (month === 5 && day >= 21)) {
     return astrologySigns[2];
-  } else if ((month == 7 && day < 23) || (month == 6 && day >= 21)) {
+  } else if ((month === 7 && day < 23) || (month === 6 && day >= 21)) {
     return astrologySigns[3];
-  } else if ((month == 8 && day < 23) || (month == 7 && day >= 23)) {
+  } else if ((month === 8 && day < 23) || (month === 7 && day >= 23)) {
     return astrologySigns[4];
-  } else if ((month == 9 && day < 23) || (month == 8 && day >= 23)) {
+  } else if ((month === 9 && day < 23) || (month === 8 && day >= 23)) {
     return astrologySigns[5];
-  } else if ((month == 10 && day < 23) || (month == 9 && day >= 23)) {
+  } else if ((month === 10 && day < 23) || (month === 9 && day >= 23)) {
     return astrologySigns[6];
-  } else if ((month == 11 && day < 22) || (month == 10 && day >= 23)) {
+  } else if ((month === 11 && day < 22) || (month === 10 && day >= 23)) {
     return astrologySigns[7];
-  } else if ((month == 12 && day < 22) || (month == 11 && day >= 23)) {
+  } else if ((month === 12 && day < 22) || (month === 11 && day >= 23)) {
     return astrologySigns[8];
   }
 }
@@ -127,40 +128,13 @@ function randGen() {
 function sentence() {
   let rand1 = Math.floor(Math.random() * 10);
   let rand2 = Math.floor(Math.random() * 10);
+
   let rand3 = Math.floor(Math.random() * 10);
   let rand4 = Math.floor(Math.random() * 10);
   let rand5 = Math.floor(Math.random() * 10);
   let rand6 = Math.floor(Math.random() * 10);
-  let i = randGen();
-  let content =
-    "The " +
-    adjectives[rand1] +
-    " " +
-    nouns[rand2] +
-    " " +
-    adverbs[rand3] +
-    " " +
-    verbs[rand4] +
-    " because some " +
-    nouns[rand1] +
-    " " +
-    adverbs[rand1] +
-    " " +
-    verbs[rand1] +
-    " " +
-    preposition[rand1] +
-    " a " +
-    adjectives[rand2] +
-    " " +
-    nouns[rand5] +
-    " which, became a " +
-    adjectives[rand3] +
-    ", " +
-    adjectives[rand4] +
-    " " +
-    nouns[rand6] +
-    ".";
-  return content;
+  randGen();
+  return `The ${adjectives[rand1]} ${nouns[rand2]} ${adverbs[rand3]} ${verbs[rand4]} because some ${nouns[rand1]} ${adverbs[rand1]} ${verbs[rand1]} ${preposition[rand1]} a ${adjectives[rand2]} ${nouns[rand5]} which, became a ${adjectives[rand3]}, ${adjectives[rand4]} ${nouns[rand6]}.`;
 }
 console.log(`Your zodiac message is ${sentence()}`);
 
